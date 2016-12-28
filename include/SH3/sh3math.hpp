b49843c6d6248b2088713a487cb2393b5da50724
Quaker762 <Jooster669@gmail.com>
2016-12-28 22:33:29 +1100

    First attempt at inline assembly (fuaaaaarkk)

--- include/SH3/sh3math.hpp
+++ include/SH3/sh3math.hpp
@@ -15,6 +15,7 @@ Author:
 Environment:
 
 Notes:
+    http://neilkemp.us/src/sse_tutorial/sse_tutorial.html
 
 Revision History:
         17-12-2016: File Created [Quaker762]
@@ -52,6 +53,48 @@ static inline int __check_sse3()
 /*                                                          */
 /************************************************************/
 
+/*++
 
+Routine Description:
+        Add two vectors using SSE3 Registers and operations
+
+Arguments:
+        T& v1 - Reference to the first vector/vertex
+        T& v2 - Reference to second vector/vertex
+
+Return Type:
+        typename T
+
+--*/
+template<typename T> static T __sse_vector_add(const T& v1, const T& v2)
+{
+    T ret = {0, 0, 0, 0}; // This is pushed onto the stack first (ESP+12)
+
+    __asm__
+    (
+        "MOV EBP, ESP\n"
+        "MOV EAX, [EBP+4]\n"               // Store pointer to v1 in EBX
+        "MOV EBX, [EBP+8]\n"               // Store pointer to v2 in EBX
+        "MOVUPS XMM0, [EAX]\n"
+        "MOVUPS XMM1, [EBX]\n"
+        "ADDPS XMM0, XMM1\n"
+        "MOVUPS [ESP+12], XMM0\n"
+    );
+
+    return ret;
+}
+
+static int __test_asm(int x)
+{
+    int y = 0;
+
+    __asm__
+    (
+        "movw EAX, [EBP+8]\n"   // x is here
+        "movw [ESP+12], EAX"    // y is here
+    );
+
+    return y;
+}
 
 #endif // SH3MATH_HPP_INCLUDED
