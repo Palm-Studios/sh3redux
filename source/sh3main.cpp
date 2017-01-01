c12e75cd10fdf49945e0e9373ea955481d5b8e98
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-01-01 11:34:15 +0100

    Use __attribute__((vector_size())) to leverage SSE
    This removes the inline asm and can generate code for more modern, as
    well as older architectures.
    To ensure that the binary builds with SSE, set CMAKE_CXX_FLAGS to
    '-msse' when generating a makefile.

--- source/sh3main.cpp
+++ source/sh3main.cpp
@@ -27,10 +27,32 @@ Revision History:
 #include <SH3/system/sh3_config.hpp>
 #include <iostream>
 
-typedef struct
+struct test
 {
+    friend test operator*(const test& v1, const test& v2);
+    friend test operator+(const test& v1, const test& v2);
+
+private:
+    using vector = float __attribute__((vector_size(16)));
+    const vector& as_vec() const { return reinterpret_cast<const vector&>(*this); }
+    test(const vector& v): test(v[0], v[1], v[2], v[3]) {}
+
+public:
+    test(): test(0, 0, 0, 0) {}
+    test(float a, float b, float c, float d): x(a), y(b), z(c), w(d) {}
+
     float x, y, z, w;
-} test;
+};
+
+test operator*(const test& v1, const test& v2)
+{
+    return v1.as_vec() * v2.as_vec();
+}
+
+test operator+(const test& v1, const test& v2)
+{
+    return v1.as_vec() + v2.as_vec();
+}
 
 /*++
 
@@ -54,12 +76,16 @@ SHSTATUS main(int argc, char** argv)
     Log(LOG_INFO, "Copyright 2016-2017 Palm Studios\n");
 
     test t1 = {1, 2, 3, 4};
-    test t2 = {0, 2, 0, 0};
+    test t2 = {0, 3, 0, 0};
     test ret;
 
-    ret = __sse_vector_muls(&t1, &t2);
+    ret = t1 * t2;
 
-    printf("%f", ret.y);
+    printf("%f\n", ret.y);
+
+    ret = t1 + t2;
+
+    printf("%f\n", ret.y);
 
 //    sh3_config config;
 //    sh3_arc arc;
