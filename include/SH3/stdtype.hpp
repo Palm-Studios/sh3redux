1659f2ca49911a3a4d68b99124fbd1460bf48809
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-01-02 19:44:05 +0100

    Remove unused code from stdtype.h
    Using windows.h is not very cross-platform, despite what the comment
    stated.
    
    SH{FALSE,TRUE} is unnecessary due to C++'s bool type.
    
    I'm not exactly sure how BADALLOC was supposed to be used. malloc and
    operator new are defined to return a nullptr if allocation fails.
    Some research of the value 0xBAADF00D it was defined to for Windows
    suggests that this value is used for uninitialized memory. I don't think
    anyone is going to check if a variable contains 0xBAADF00D before
    reading it though, and additionally compilers may do funky stuff if they
    can prove you're reading uninitialized memory, which is undefined
    behavior.
    Instead we should compile and run with the sanitizers (ASan, MSan,
    UBSan, ect.) on occasion. Static analysis, like compiler warnings at the
    least, should be able to warn about some some uninitialized reads as
    well.

--- include/SH3/stdtype.hpp
+++ include/SH3/stdtype.hpp
@@ -26,21 +26,7 @@ Revision History:
 #ifndef STDTYPE_HPP_INCLUDED
 #define STDTYPE_HPP_INCLUDED
 
-#include <cstdio>
-#include <cstring>
-#include <cstdarg>
-
 #include "SH3/system/log.hpp"
-
-    // Cross platform ;)
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
-#elif __APPLE__
-
-#elif __linux__
-
-#endif
 
 /************************************************************/
 /*                   TYPE DEFINITIONS                       */
@@ -48,26 +34,8 @@ Revision History:
 /* the Engine                                               */
 /************************************************************/
 
-#define SHFALSE 0
-#define SHTRUE  1
-
 #define SH_OK   0
 #define SH_BAD  -1
-
-/**++
-    These allow us to check more specifically for a bad allocation across multiple systems
-    though the compiler/stdlib really should take care of it for us
-
-    This is probably really bad programming, but it makes it a bit easier to read.
-
-    (Is this only for programs with Debugging symbols enabled?!?!)
---**/
-
-#ifdef _WIN32
-    #define BADALLOC (void*)0xBAADF00D; // This is the nullptr on Windows Systems (I think...)
-#else
-    #define BADALLOC (void*)0; // Not sure what Linux/OSX define this as...
-#endif
 
 typedef int SHSTATUS; // Return type for functions
 
