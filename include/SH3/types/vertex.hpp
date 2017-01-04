a6a44eef6727f99d877484b468b4c2379a3b6589
Quaker762 <Jooster669@gmail.com>
2017-01-05 00:18:28 +1100

    Added documentation to 'vertex.hpp'
    Forgot this one... Oops....

--- include/SH3/types/vertex.hpp
+++ include/SH3/types/vertex.hpp
@@ -1,54 +1,43 @@
-/*++
-
-Copyright (c) 2016  Palm Studios
-
-Module Name:
-        Macros and structures related to an OpenGL 3D vertex
-
-Abstract:
-
-Author:
-        Jesse Buhagiar
-
-Environment:
-
-Notes:
-
-Revision History:
-        14-12-2016: Initial Revision [jbuhagiar]
---*/
-
+/** @file
+ *  Macros and structures related to an OpenGL 3D vertex.
+ *
+ *  @copyright 2016  Palm Studios
+ *
+ *  @date 14-12-2016
+ *
+ *  @author Jesse Buhagiar
+ */
 #ifndef VERTEX_HPP_INCLUDED
 #define VERTEX_HPP_INCLUDED
 
 #include <GL/gl.h>
 
-// Basic vertex type
+/** Basic vertex type */
 typedef struct
 {
-    GLfloat x, y, z;      // Position
+    GLfloat x, y, z;      /**< Position */
 } vertex3f_t;
 
-// Textured Vertex with normals
+/** Textured Vertex with normals */
 typedef struct
 {
     Glfloat x, y, z;
-    Glfloat u, v;         // Texture UV co-ords
-    GLfloat nx, ny, nz;   // Vertex normals (do we need 3??)
+    Glfloat u, v;         /**< Texture UV co-ords */
+    GLfloat nx, ny, nz;   /**< Vertex normals (do we need 3??) */
 } vertex3f_tex_t;
 
-// Basic vertex type
+/** Basic vertex type */
 typedef struct
 {
-    GLfloat x, y, z, w;      // Position
+    GLfloat x, y, z, w;      /**< Position */
 } vertex4f_t;
 
-// Textured Vertex with normals
+/** Textured Vertex with normals */
 typedef struct
 {
     float x, y, z, w;
-    float u, v;         // Texture UV co-ords
-    float nx, ny, nz;   // Vertex normals (do we need 3??)
+    float u, v;         /**< Texture UV co-ords */
+    float nx, ny, nz;   /**< Vertex normals (do we need 3??) */
 } vertex4f_tex_t;
 
 #endif // VERTEX_HPP_INCLUDED
