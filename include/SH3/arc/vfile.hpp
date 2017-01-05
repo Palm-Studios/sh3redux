417a58846a5bd318139d121afde7425d550b57b1
quaker762 <jooster669@gmail.com>
2017-01-06 01:43:12 +1100

    Initial implementation of Virtual Files
    Title explains it all. These are supposed to somewhat mimic C file IO,
    as some file types will need to be seeked (Mesh files, animation files
    etc). I think this makes a nice way to handle Virtual Files from arc
    sections.
    
    However, I feel the std::vector buffer may be a waste of space, and
    there could be a MUCH better way to do this without buffering the entire
    file (a HUGE waste of memory).

---
+++ include/SH3/arc/vfile.hpp
@@ -0,0 +1,98 @@
+/** @file
+ *
+ *  Class and data structures describing a Virtual File inside a SILENT HILL 3 arc section.
+ *
+ *  @copyright 2016  Palm Studios
+ *
+ *  @date 5-1-2017
+ *
+ *  @author Jesse Buhagiar
+ */
+#ifndef VFILE_HPP_INCLUDED
+#define VFILE_HPP_INCLUDED
+
+#include "SH3/arc/types.hpp"
+#include "SH3/error.hpp"
+#include <ios>
+
+/**
+ *  Opens a handle to a Virtual File contained within arc.arc
+ *
+ *  After the handle to the file is open, it is possible to seek and rewind
+ *  to a specific part of the file using @ref Seek().
+ *
+ *  Each read sets the @ref read_error indicating whether a partial read was performed,
+ *  or that the end of file was encountered (meaning that @ref fpos `+ len` was equal to @ref fsize.
+ *
+ */
+struct sh3_arc_vfile final
+{
+public:
+    /**
+     *  Failure codes for @ref ReadData.
+     */
+    enum class load_result
+    {
+        SUCCESS,
+        END_OF_FILE,
+        PARTIAL_READ,
+    };
+
+    struct read_error final : public error<load_result>
+    {
+    public:
+        std::string message() const;
+    };
+
+
+    sh3_arc_vfile(sh3_arc& mft, const std::string& fname): fpos(0)
+    {Open(mft, fname);}
+
+    /**
+     *  Read @c len bytes of data into a destination buffer.
+     *
+     *  @param destination      Pointer to write read values into.
+     *  @param len              Number of bytes to read from the file.
+     *  @param e                @ref read_error from this operation.
+     *
+     *  @returns Number of bytes read (which should be len)
+     */
+    std::size_t ReadData(void* destination, std::size_t len, read_error& e);
+
+    /**
+     *  Rewind this file to the beginning (set fpos to 0).
+     */
+    void Rewind(){fpos = 0;};
+
+    /**
+     *  Seek to a certain position in the file.
+     *
+     *  @param pos      Position to seek to.
+     *  @param origin   The origin we want to seek from.
+     */
+     void Seek(long pos, std::ios_base::seekdir origin);
+
+
+private:
+    std::uint32_t   fpos;           /**< Current file position */
+    std::size_t     fsize;          /**< Size of this file inside the arc section in bytes */
+    std::string     fname;          /**< The name of this file (taken from arc.arc) */
+    bool            open = false;   /**< Is this file handle currently open? */
+
+    std::vector<std::uint8_t> buffer; /**< Buffer that @ref ReadData() reads from */
+
+    /**
+     *  Open a handle to a virtual file.
+     *
+     *  @param mft      The @ref sh3_arc Master File Table, arc.arc.
+     *  @param fname    The name of the file we want to open.
+     *
+     *  @note If the file is already open, this function returns false.
+     *
+     *  @returns @c true if the file was found, @c false if not.
+     */
+    bool Open(sh3_arc& mft, const std::string& fname);
+};
+
+
+#endif // VFILE_HPP_INCLUDED
