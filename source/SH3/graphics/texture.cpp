Palm-Studios/sh3redux

commit a1f9f378edd9b13ecc43d3109823e02ddf8ae982

Use error for sh3_texure::load_result.

source/SH3/graphics/texture.cpp
@@ -14,6 +14,24 @@
 
 using namespace sh3_graphics;
 
+std::string texture::load_error::message() const
+{
+    std::string error;
+    switch(result)
+    {
+    case load_result::SUCCESS:
+        error = "Success";
+        break;
+    case load_result::INVALID_CHUNK:
+        error = "Invalid chunk";
+        break;
+    case load_result::END_OF_FILE:
+        error = "End of file";
+        break;
+    }
+    return error;
+};
+
 texture::load_result sh3_graphics::texture::Load(const std::string& filename, sh3_arc& arc)
 {
     if(arc.LoadFile(filename, pixbuff) == ARC_FILE_NOT_FOUND)
@@ -21,5 +39,5 @@ texture::load_result sh3_graphics::texture::Load(const std::string& filename, sh
         die("sh3_graphics::texture::Load( ): Unable to find file %s!", filename.c_str());
     }
 
-    return load_result::TEOF;
+    return load_result::END_OF_FILE;
 }

include/SH3/graphics/texture.hpp
@@ -22,6 +22,8 @@
 #include <vector>
 #include <GL/gl.h>
 
+#include "SH3/error.hpp"
+
 namespace sh3_graphics
 {
     /** @defgroup graphics-headers Graphics headers
@@ -75,8 +77,15 @@ namespace sh3_graphics
          */
         enum class load_result
         {
+            SUCCESS,
             INVALID_CHUNK,
-            TEOF,
+            END_OF_FILE,
+        };
+
+        struct load_error final : public error<load_result>
+        {
+        public:
+            std::string message() const;
         };
 
         texture(){}
@@ -91,7 +100,7 @@ namespace sh3_graphics
          * @return @ref load_result indicating whether the read failed or not
          *
          */
-        load_result Load(const std::string& filename, sh3_arc& arc);
+        load_error Load(const std::string& filename, sh3_arc& arc);
 
     private:
         static constexpr std::uint32_t INVALID_TEXTURE = std::numeric_limits<uint32_t>::max();

commit c731f87efc754b143b8409acdde354870b42fdd9

Fix sh3_graphics::texture::Load signature in implementation
This error was introduced in a1f9f378edd9b13ecc43d3109823e02ddf8ae982.

source/SH3/graphics/texture.cpp
@@ -32,12 +32,16 @@ std::string texture::load_error::message() const
     return error;
 };
 
-texture::load_result sh3_graphics::texture::Load(const std::string& filename, sh3_arc& arc)
+texture::load_error sh3_graphics::texture::Load(const std::string& filename, sh3_arc& arc)
 {
+    load_error error;
+
     if(arc.LoadFile(filename, pixbuff) == ARC_FILE_NOT_FOUND)
     {
         die("sh3_graphics::texture::Load( ): Unable to find file %s!", filename.c_str());
     }
 
-    return load_result::END_OF_FILE;
+    error.set_error(load_result::END_OF_FILE);
+
+    return error;
 }