b44380d00f00e00612c8d959057cb33ee5dede5d
Quaker762 <Jooster669@gmail.com>
2017-01-28 00:20:59 +1100

    Initial Implementation of Shader Programs
    Can load, compile and link shaders from files. Currently untested (as we
    don't have a GLContext working as of yet!)

--- source/main.cpp
+++ source/main.cpp
@@ -11,35 +11,9 @@
 #include "SH3/system/exit_code.hpp"
 #include "SH3/system/log.hpp"
 #include "SH3/system/window.hpp"
+#include "SH3/system/glprogram.hpp"
 #include <cstdio>
 
-struct test
-{
-    friend test operator*(const test& v1, const test& v2);
-    friend test operator+(const test& v1, const test& v2);
-
-private:
-    using vector = float __attribute__((vector_size(16)));
-    const vector& as_vec() const { return reinterpret_cast<const vector&>(*this); }
-    // cppcheck-suppress noExplicitConstructor
-    test(const vector& v): test(v[0], v[1], v[2], v[3]) {}
-
-public:
-    test(): test(0, 0, 0, 0) {}
-    test(float a, float b, float c, float d): x(a), y(b), z(c), w(d) {}
-
-    float x, y, z, w;
-};
-
-test operator*(const test& v1, const test& v2)
-{
-    return v1.as_vec() * v2.as_vec();
-}
-
-test operator+(const test& v1, const test& v2)
-{
-    return v1.as_vec() + v2.as_vec();
-}
 
 /**
  *  Entry point to the program.
@@ -55,29 +29,13 @@ int main(int argc, char** argv)
 {
     static_cast<void>(argc);
     static_cast<void>(argv);
+
     Log(LogLevel::INFO, "===SILENT HILL 3 REDUX===");
     Log(LogLevel::INFO, "Copyright 2016-2017 Palm Studios\n");
 
-    test t1 = {1, 2, 3, 4};
-    test t2 = {0, 3, 0, 0};
-    test ret;
+    sh3_window window(640, 480, "sh3redux");
+    sh3_graphics::sh3_glprogram program("test");
 
-    ret = t1 * t2;
-
-    std::printf("%f\n", static_cast<double>(ret.y));
-
-    ret = t1 + t2;
-
-    std::printf("%f\n", static_cast<double>(ret.y));
-
-//    sh3_config config;
-//    sh3_arc arc;
-//
-//    arc.Load();
-//    std::vector<uint8_t> data;
-//    arc.LoadFile("data/eff_tex/fire00_tr.pic", data);
-//
-//    sh3_window* window = new sh3_window(640, 480, "SILENT HILL 3");
 
     return static_cast<int>(exit_code::SUCCESS);
 }
