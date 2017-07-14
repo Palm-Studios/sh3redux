1c762f9051b81ebcda50073b86a99087634ba689
Quaker762 <Jooster669@gmail.com>
2017-07-14 21:53:56 +1000

    Added way to calculate Projection Matrix
    Added functionality in the OpenGL context to calculate the current
    Projection Matrix according to the Camera's FOV and the current Window's
    dimensions, as well as being able to set the near and far values of the
    current view frustum (handy for the outdoor Silent Hill level of SH3 and SH2). This may eventually need to be moved, as I don't really see a point in passing the window to any game logic (perhaps to change the Window settings in game, though this is unlike the original).

--- include/SH3/system/glcontext.hpp
+++ include/SH3/system/glcontext.hpp
@@ -1,7 +1,7 @@
 /** @file
  *  Defines a logical OpenGL Context
  *
- *  @copyright 2016  Palm Studios
+ *  @copyright 2016-2017 Palm Studios
  *
  *  @date 22-12-2016
  *
@@ -15,57 +15,76 @@
 #include <memory>
 #include <vector>
 #include <GL/glew.h>
+#include <glm/glm.hpp>
+
 #include "SH3/system/sdl_destroyer.hpp"
 
 class sh3_window;
 
-class sh3_glcontext
+namespace sh3_gl
 {
-public:
-    explicit sh3_glcontext(sh3_window& hwnd);
+    class context
+    {
+    public:
+        explicit context(sh3_window& hwnd);
 
-    /**
-     *  Return the vendor of OpenGL Driver in use.
-     */
-    const char* GetVendor() const;
+       /**
+        *  Return the vendor of OpenGL Driver in use.
+        */
+        const char* GetVendor() const;
 
-    /**
-     *  Get the version string of the OpenGL context currently in use.
-     *
-     *  @return OpenGL Version String.
-     */
-    const char* GetVersion() const;
+       /**
+        *  Get the version string of the OpenGL context currently in use.
+        *
+        *  @return OpenGL Version String.
+        */
+        const char* GetVersion() const;
 
-    /**
-     *  Get the name of the renderer.
-     *
-     *  @return OpenGL Renderer name (Graphics Card name)
-     */
-    const char* GetRenderer() const;
+       /**
+        *  Get the name of the renderer (Graphics Card that created this context).
+        *
+        *  @return OpenGL Renderer name (Graphics Card name).
+        */
+        const char* GetRenderer() const;
 
-    /**
-     *  Get a list of all extensions supported by the user's graphics card.
-     */
-    void GetExtensions();
+       /**
+        *  Get a list of all extensions supported by the user's graphics card.
+        */
+        void GetExtensions();
 
-    /**
-     *  Print out information about the current GL Context.
-     */
-    void PrintInfo() const;
+       /**
+        *  Print out information about the current GL Context.
+        */
+        void PrintInfo() const;
 
-private:
+       /**
+        *   Calculate our projection matrix (how we project vertices on the Z plane on the 2D screen).
+        *   Note that OpenGL has +Z coming TOWARDS the user, which is slightly unintuitive, as it means
+        *   each vertex must have its' Z value inverted.
+        *
+        *   @param fov - The Field Of View (FOV) of our Viewing Frustum. Defined in @ref sh3_graphics::camera.
+        *   @param near - The near plane of our Viewing Frustum, i.e, when objects will disappear.
+        *   @param far - The far plane of our Viewing Frustum, i.e, when far objects will disappear (SH fog etc).
+        *
+        *   @return glm::mat4 - 4 float matrix we send to our GLSL program.
+        */
+        glm::mat4 GetProjectionMatrix(float fov, int width, int height, float near, float far);
 
-    /**
-     *  Get a string from the current GL Context.
-     *
-     *  @return String from @c glGetString
-     */
-    static const char* GlGetString(GLenum name);
+    private:
 
-private:
-    std::unique_ptr<flat_sdl_glcontext, sdl_destroyer> glContext;
-    std::vector<const char*> extensions;
+       /**
+        *  Get a string from the current GL Context.
+        *
+        *  @param name - Name of symbolic constant we want to get from the driver.
+        *
+        *  @return char* from @c glGetString.
+        */
+        static const char* GlGetString(GLenum name);
 
-};
+    private:
+        std::unique_ptr<flat_sdl_glcontext, sdl_destroyer> glContext;   /**< A pointer to our actual OpenGL Context */
+        std::vector<const char*> extensions;                            /**< List of all extensions supported by this OpenGL Driver */
+    };
+}
 
 #endif // SH3_GLCONTEXT_HPP_INCLUDED
--- include/SH3/system/window.hpp
+++ include/SH3/system/window.hpp
@@ -25,10 +25,11 @@ private:
 public:
     sh3_window(int width, int height, const std::string& title);
 
-    std::unique_ptr<SDL_Window, sdl_destroyer> hwnd;
+    std::unique_ptr<SDL_Window, sdl_destroyer> hwnd;        /**< Our window handle */
+    sh3_gl::context context;                                /**< This window's OpenGL Context */
 
 private:
-    sh3_glcontext context; /**< This window's OpenGL Context */
+
 };
 
 #endif // SH3_WINDOW_HPP_INCLUDED
