fc84af4ed2e18a0550bc35524e3fe26ddc5041bd
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-08-20 21:16:41 +0200

    Export .arc structure documentation
    Also applies some minor phrasing changes.

--- include/SH3/arc/types.hpp
+++ include/SH3/arc/types.hpp
@@ -1,37 +1,9 @@
 /** @file
- *  Describes the structures necessary to load/read Silent Hill 3 .arc files
- *  (including @c arc.arc, the master file table).
+ *  Describes the structures necessary to load/read Silent Hill 3 sub-arc files.
  *
- *   The structure of the .arc system is as follows:
+ *  @see @ref arc-files
  *
- *                   arc.arc (MFT)
- *                       |
- *                       | (File name translation to section name)
- *           /-----/-----|-----\-----\
- *          |     |      |      |     |
- *       pic.arc  chrbg.arc msg.arc tex.arc
- *          |         |        |       |    (Translation to section offset)
- *          |         |        |       |
- *         File      File     File    File
- *
- *   The files branching from the MFT are NOT compressed, however, @c arc.arc is compressed using
- *   gzip compression format. @c arc.arc basically acts as a Master File Table (or rather a root directory).
- *
- *   It is probably a good idea to (at launch) load the MFT and map each file in a map (see @ref sh3_arc_section::fileList)
- *   so that we can quickly look up and load a file in a section without having to transverse the MFT everytime,
- *   though it can be made quicker by skipping sections (which is most likely how Konami implemented it).
- *
- *   After we have a handle to @c arc.arc, we can now load each section (as Mike has designated them) or as I like
- *   to call them (and perhaps more appropriately), sub arcs. These are the files found in @c /data/ of a regular
- *   install of SILENT HILL 3 on the PC. These sections contain information about each file contained within,
- *   such as a Virtual File Path (e.g @c /data/pic/it/it_xxxx.tex, then translated to an offset), the offset
- *   within the section it is located in, as well as the index of the section (for reasons that elude me).
- *
- *   It is currently unknown how the programmers at Konami specified which files should be loaded when, but
- *   considering the very quick load times, I would assume that each part of the game is in its own individual
- *   sub arc/section, as Mike seems to have not found
- *
- *  @copyright 2016  Palm Studios and Mike M (<a href="https://twitter.com/perdedork">\@perdedork</a>)
+ *  @copyright 2016-2017 Palm Studios and Mike M (<a href="https://twitter.com/perdedork">\@perdedork</a>)
  *
  *  @date 14-12-2016
  *
@@ -46,7 +18,7 @@
 #include <vector>
 #include <zlib.h>
 
-#define ARC_FILE_NOT_FOUND  -1          /**< Status @ref sh3_arc::LoadFile() returns if a file cannot be found */
+#define ARC_FILE_NOT_FOUND  -1 /**< Status @ref sh3_arc::LoadFile() returns if a file cannot be found */
 
 namespace sh3 { namespace arc {
     struct mft;
