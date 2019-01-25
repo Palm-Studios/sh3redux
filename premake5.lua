2488a11f61d47d9b5d97a0bee9017ffba1e18b86
Jesse Buhagiar <jooster669@gmail.com>
2019-01-25 23:53:40 +1100

    Complete overhaul of build system
    Build system has been overhauled, especially for systems that do NOT
    have a package manager installed (i.e Windows). The `libs` folder
    contains the most up to date versions of libraries we are using, which
    are known to be WORKING. This way (at least for Windows builds), we can
    control versioning of our dependencies much better instead of having the
    user have to fetch them. The premake5 file also takes into account which
    system the user is on, meaning we can link linux versions of the
    libraries, as WELL as windows versions.
    
    Premake is also much less of a fuck around, and the simple `.lua` script
    is very simple to modify and understand.

---
+++ premake5.lua
@@ -0,0 +1,52 @@
+-- premake5.lua for SILENT HILL 3: Redux
+-- Based on the example one found on Github
+
+workspace "silenthill3"
+    architecture "x86_64"
+    configurations {"Debug", "Release", "Distro"}
+    language "C++"
+
+project "sh3r"
+    kind "ConsoleApp"
+    targetdir ("build/sh3r/%{cfg.buildcfg}_%{cfg.archtecture}")
+    objdir ("obj/sh3r/%{cfg.buildcfg}_%{cfg.archtecture}")
+    files {"source/SH3/**.cpp", "source/main.cpp", "include/SH3/**.hpp"}
+
+    
+    includedirs {"include", "third_party/debugbreak"}
+
+    
+    filter {"system:windows"}
+        libdirs {"libs/SDL2-2.0.9/build", "libs/zlib-1.2.11/build", "libs/glew-2.1.0/build/lib"}
+        links {"zlibstatic", "glew32", "mingw32", "SDL2main", "SDL2"}
+        sysincludedirs {"libs/SDL2-2.0.9/include", "libs/glew-2.1.0/include", "libs/boost_1_69_0", "libs/glm/include"}
+        links{"OpenGL32"}
+
+    filter {"system:not windows"}
+        libdirs {"libs/SDL2-2.0.9/build", "libs/zlib-1.2.11/build", "libs/glew-2.1.0/build/lib"}
+        sysincludedirs {"libs/SDL2-2.0.9/include", "libs/glew-2.1.0/include", "libs/boost_1_69_0", "libs/glm/include"}
+        links {"z", "SDL2", "GLEW"}
+        links {"GL"}
+
+    filter "configurations:Debug"
+        defines {"SH3_DEBUG"}
+        symbols "On"
+        cppdialect "C++17"
+        staticruntime "On"
+        buildoptions {"-Wall", "-Wextra", "-pedantic", "-Wsign-compare", "-Wold-style-cast", "-Wdeprecated", "-Wconversion", "-Wnon-virtual-dtor", "-Wundef", "-Wfloat-equal", "-Wunreachable-code", "-Weffc++"}   
+
+    filter "configurations:Release"
+        defines {""}
+        symbols "Off"
+        cppdialect "C++17"
+        staticruntime "On"
+        buildoptions {"-Wall", "-Wextra", "-pedantic", "-Wsign-compare", "-Wold-style-cast", "-Wdeprecated", "-Wconversion", "-Wnon-virtual-dtor", "-Wundef", "-Wfloat-equal", "-Wunreachable-code", "-Weffc++"}
+
+    filter "configurations:Distro"
+        defines {""}
+        symbols "Off"
+        cppdialect "C++17"
+        staticruntime "On"
+        buildoptions {"-Wall", "-Wextra", "-pedantic", "-Wsign-compare", "-Wold-style-cast", "-Wdeprecated", "-Wconversion", "-Wnon-virtual-dtor", "-Wundef", "-Wfloat-equal", "-Wunreachable-code", "-Weffc++"}
+
+
