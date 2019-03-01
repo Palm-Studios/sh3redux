cf92e8cde1e3bf0fbcea4466a8f8d662c6ce6007
Jesse Buhagiar <jooster669@gmail.com>
2019-03-01 23:16:12 +1100

    Basic state based game system
    A very barebones implementation of a state based game system. The game
    itself is modelled as a series of 'states', a state being an object that
    contains all code and data required for a particular 'screen' the game
    is in (for example, all 3D geometry data would be loaded in the 'game'
    scene, while all inventory data would be contained in the 'inventory'
    screen).
    
    As everything in the original SH3 is hardcoded, this
    _significantly_ reduces the amount of code required, being that we can
    just  feed a scene name (e.g `mr3f) into the state and have it worry
    about loading everything.
    
    There is a lot that could be refactored here, as the code is a little
    bit janky, but for now this works.

---
+++ source/SH3/engine/statemanager.cpp
@@ -0,0 +1,102 @@
+/** @file
+ *
+ *  Implementation of statemanager.cpp
+ *
+ *  @copyright 2016-2019  Palm Studios
+ *
+ *  @date 10-2-2019
+ *
+ *  @author Jesse Buhagiar [quaker762]
+ */
+#include "SH3/engine/statemanager.hpp"
+#include "SH3/system/log.hpp"
+
+#include <iostream>
+
+using namespace sh3::state;
+
+class StackProtector : public CGameState
+{
+public:
+
+    StackProtector(CStateManager& mgr) : CGameState(mgr)
+    {
+        name = "GUARD_STATE";
+        id = 0;
+    }
+
+
+    /**
+     *
+     */
+    ~StackProtector()
+    {
+    }
+
+    virtual std::unique_ptr<CGameState> clone() const override
+    {
+        return std::make_unique<StackProtector>(*this);
+    }
+
+    virtual void Init(void) noexcept
+    {
+        // Do nothing
+    }
+
+    virtual void Destroy(void) noexcept
+    {
+        die("BLERGH! Call to StackProtector::Destroy (you've tried to underflow the stack!!)");
+    }
+
+    virtual void Update(void) noexcept
+    {
+        std::cout << "TICK ";
+    }
+
+    virtual void Render(void) noexcept
+    {
+        std::cout << "TOCK" << std::endl;
+    }
+
+    virtual void InputHandler(void) noexcept
+    {
+        // Do nothing
+    }
+};
+
+CStateManager::CStateManager(void)
+    : stateStack(), numStates(0)
+{
+    StackProtector protect(*this);
+
+    PushState(protect);
+}
+
+CStateManager::~CStateManager(void)
+{
+    // Unwind the stack
+    for(std::size_t i = 0; i < stateStack.size(); i++)
+    {
+        stateStack.top().get()->Destroy();
+        stateStack.pop();
+    }
+}
+
+void CStateManager::PushState(const CGameState& state)
+{
+    numStates++;
+    stateStack.push(state.clone());
+    stateStack.top().get()->Init();
+}
+
+void CStateManager::PopState(void)
+{
+    numStates--;
+    stateStack.top().get()->Destroy();
+    stateStack.pop();
+}
+
+const std::unique_ptr<CGameState>& CStateManager::Peek(void) const
+{
+    return stateStack.top();
+}
